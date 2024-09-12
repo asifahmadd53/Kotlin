bff464ec58cc299a3c298fd3ee3b8c9eda2a649f
asifahmadd53 <asifahmadd53@gmail.com>
2024-09-12 10:06:34 +0500

    operators

---
+++ src/main/kotlin/operators.kt
@@ -0,0 +1,28 @@
+fun main (){
+    var i= 3;
+    var j = 5;
+
+    println(i+j)
+    println(i-j)
+    println(i*j)
+    println(i/j)
+    println(i%j)
+
+    println(i>j)
+    println(i<j)
+    println(i<=j)
+    println(i>=j)
+    println(i!= j)
+
+    // increament operator
+
+    var k =10;
+    k++
+
+    // ++i is the pre increment
+    // i++ post increment
+
+    ++k
+
+    println(k)
+}
