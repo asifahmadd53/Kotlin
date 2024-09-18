4b522627738b5b867409bd6361b34c0a685d4acb
asifahmadd53 <asifahmadd53@gmail.com>
2024-09-18 23:54:54 +0500

    classes and objets

---
+++ src/main/kotlin/Classes.kt
@@ -0,0 +1,21 @@
+package main.kotlin
+
+fun main() {
+    val mustang = Car("Mustang", "petrol", kmRun = 1000)
+    val beetle = Car("Beetle", "diesel", kmRun = 500)
+    println(mustang.name)
+    println(mustang.type)
+
+    println(beetle.name)
+    mustang.driveCar()
+}
+
+class Car(val name: String, val type: String, val kmRun: Int) {
+    fun driveCar() {
+        println("The $name is driving")
+    }
+
+    fun applyBrakes() {
+        println("Brakes applied")
+    }
+}
